1c8d3a4c675276f578a14e0621fd3f80ffed2e6b
Bruno Sgotti <71176979+sgottera98@users.noreply.github.com>
2021-03-17 10:22:54 -0300

    front

---
+++ src/main/App.jsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Routes from './Routes'
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Footer from '../components/template/Footer'
+
+export default props => 
+    <Router>
+        <div className="app">
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </Router>
